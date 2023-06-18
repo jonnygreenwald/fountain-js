90a5625e03daeafa9ec01be0d9e3e3316e8aceb3
Chandler Anderson <chanderson90@gmail.com>
2023-06-18 18:51:40 -0400

    Separate each token type's algorithms into separate functions

--- src/scanner.ts
+++ src/scanner.ts
@@ -4,136 +4,194 @@ import { Token } from './token';
 import { Lexer } from './lexer';
 
 export class Scanner {
-    private tokens: Token[] = [];
+    private lastLineWasDualDialogue: boolean;
 
     tokenize(script: string): Token[] {
         // reverse the array so that dual dialog can be constructed bottom up
         const source: string[] = new Lexer().reconstruct(script).split(regex.splitter).reverse();
 
-        let line: string;
-        let match: string[];
-        let dual: boolean;
-
-        for (line of source) {
+        const tokens: Token[] = source.reduce((previous: Token[], line: string) => {
             /** title page */
             if (regex.title_page.test(line)) {
-                match = line.replace(regex.title_page, '\n$1').split(regex.splitter).reverse();
-
-                for (let item of match) {
-                    let pair = item.replace(regex.cleaner, '').split(/\:\n*/);
-
-                    this.tokens.push({ type: pair[0].trim().toLowerCase().replace(' ', '_'), is_title: true, text: pair[1].trim() });
-                }
-                continue;
+                return this.tokenizeTitlePage(line, previous);
             }
-
             /** scene headings */
-            if (match = line.match(regex.scene_heading)) {
-                let text = match[1] || match[2];
-                let meta: RegExpMatchArray;
-                let num: string;
-
-                if (text.indexOf('  ') !== text.length - 2) {
-                    if (meta = text.match(regex.scene_number)) {
-                        num = meta[2];
-                        text = text.replace(regex.scene_number, '');
-                    }
-
-                    this.tokens.push({ type: 'scene_heading', text: text, scene_number: num || undefined });
-                }
-                continue;
+            if (regex.scene_heading.test(line)) {
+                return this.tokenizeSceneHeading(line, previous);
             }
-
             /** centered */
-            if (match = line.match(regex.centered)) {
-                this.tokens.push({ type: 'centered', text: match[0].replace(/>|</g, '') });
-                continue;
+            if (regex.centered.test(line)) {
+                return this.tokenizeCentered(line, previous);
             }
-
             /** transitions */
-            if (match = line.match(regex.transition)) {
-                this.tokens.push({ type: 'transition', text: match[1] || match[2] });
-                continue;
+            if (regex.transition.test(line)) {
+                return this.tokenizeTransition(line, previous);
             }
-
             /** dialogue blocks - characters, parentheticals and dialogue */
-            if (match = line.match(regex.dialogue)) {
-                let name = match[1] || match[2];
-
-                if (name.indexOf('  ') !== name.length - 2 || line.startsWith('@')) {
-                    // iterating from the bottom up, so push dialogue blocks in reverse order
-                    if (match[3]) {
-                        this.tokens.push({ type: 'dual_dialogue_end' });
-                    }
-
-                    this.tokens.push({ type: 'dialogue_end' });
-
-                    let parts: string[] = match[4].split(/(\(.+\))(?:\n+)/).reverse();
-
-                    for (let part of parts) {
-                        if (part.length > 0) {
-                            this.tokens.push({ type: regex.parenthetical.test(part) ? 'parenthetical' : 'dialogue', text: part });
-                        }
-                    }
-
-                    this.tokens.push({ type: 'character', text: name.trim() });
-                    this.tokens.push({ type: 'dialogue_begin', dual: match[3] ? 'right' : dual ? 'left' : undefined });
-
-                    if (dual) {
-                        this.tokens.push({ type: 'dual_dialogue_begin' });
-                    }
-
-                    dual = match[3] ? true : false;
-                    continue;
-                }
+            if (regex.dialogue.test(line)) {
+                return this.tokenizeDialogue(line, previous);
             }
-
             /** section */
-            if (match = line.match(regex.section)) {
-                this.tokens.push({ type: 'section', text: match[2], depth: match[1].length });
-                continue;
+            if (regex.section.test(line)) {
+                return this.tokenizeSection(line, previous);
             }
-
             /** synopsis */
-            if (match = line.match(regex.synopsis)) {
-                this.tokens.push({ type: 'synopsis', text: match[1] });
-                continue;
+            if (regex.synopsis.test(line)) {
+                return this.tokenizeSynopsis(line, previous);
             }
-
             /** notes */
-            if (match = line.match(regex.note)) {
-                this.tokens.push({ type: 'note', text: match[1] });
-                continue;
+            if (regex.note.test(line)) {
+                return this.tokenizeNote(line, previous);
             }
-
             /** boneyard */
-            if (match = line.match(regex.boneyard)) {
-                this.tokens.push({ type: match[0][0] === '/' ? 'boneyard_begin' : 'boneyard_end' });
-                continue;
+            if (regex.boneyard.test(line)) {
+                return this.tokenizeBoneyard(line, previous);
             }
-
             /** lyrics */
-            if (match = line.match(regex.lyrics)) {
-                this.tokens.push({ type: 'lyrics', text: match[0].replace(/^~(?![ ])/gm, '') });
-                continue;
+            if (regex.lyrics.test(line)) {
+                return this.tokenizeLyrics(line, previous);
             }
-
             /** page breaks */
             if (regex.page_break.test(line)) {
-                this.tokens.push({ type: 'page_break' });
-                continue;
+                return this.tokenizePageBreak(line, previous);
             }
-
             /** line breaks */
             if (regex.line_break.test(line)) {
-                this.tokens.push({ type: 'line_break' });
-                continue;
+                return this.tokenizeLineBreak(line, previous);
             }
-
             // everything else is action -- remove `!` for forced action
-            this.tokens.push({ type: 'action', text: line.replace(/^!(?![ ])/gm, '') });
+            return this.tokenizeAction(line, previous);
+        }, [])
+        
+        return tokens.reverse();
+    }
+
+    private tokenizeTitlePage(line: string, previous: Token[]): Token[] {
+        const match = line.replace(regex.title_page, '\n$1').split(regex.splitter).reverse();
+        const titlePageTokens = match.map(item => {
+            const pair = item.replace(regex.cleaner, '').split(/\:\n*/);
+            return { type: pair[0].trim().toLowerCase().replace(' ', '_'), is_title: true, text: pair[1].trim() }
+        })
+        return [...previous, ...titlePageTokens]
+    }
+
+    private tokenizeSceneHeading(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.scene_heading)
+        let text = match[1] || match[2];
+
+        if (this.isTooShort(text)) {
+            return previous;
         }
 
-        return this.tokens.reverse();
+        const meta: RegExpMatchArray = text.match(regex.scene_number);
+        let num: string;
+        if (meta) {
+            num = meta[2];
+            text = text.replace(regex.scene_number, '');
+        }
+
+        const sceneHeadingToken = { type: 'scene_heading', text: text, scene_number: num || undefined };
+        return [...previous, sceneHeadingToken];
+    }
+
+    private tokenizeCentered(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.centered)
+        const centeredToken = { type: 'centered', text: match[0].replace(/>|</g, '') };
+        return [...previous, centeredToken];
+    }
+
+    private tokenizeTransition(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.transition)
+        const transitionToken = { type: 'transition', text: match[1] || match[2] };
+        return [...previous, transitionToken]
+    }
+
+    private tokenizeDialogue(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.dialogue);
+
+        let name = match[1] || match[2];
+        if (this.isTooShort(name) && !line.startsWith('@')) {
+            return previous;
+        }
+
+        const dialogueTokens = []
+
+        // iterating from the bottom up, so push dialogue blocks in reverse order
+        const isDualDialogue = !!(match[3]);
+        if (isDualDialogue) {
+            dialogueTokens.push({ type: 'dual_dialogue_end' });
+        }
+
+        dialogueTokens.push({ type: 'dialogue_end' });
+
+        let parts: string[] = match[4].split(/(\(.+\))(?:\n+)/).reverse();
+
+        for (let part of parts) {
+            if (part.length > 0) {
+                dialogueTokens.push({ type: regex.parenthetical.test(part) ? 'parenthetical' : 'dialogue', text: part });
+            }
+        }
+
+        dialogueTokens.push({ type: 'character', text: name.trim() });
+        dialogueTokens.push({ type: 'dialogue_begin', dual: isDualDialogue ? 'right' : this.lastLineWasDualDialogue ? 'left' : undefined });
+
+        if (this.lastLineWasDualDialogue) {
+            dialogueTokens.push({ type: 'dual_dialogue_begin' });
+        }
+
+        this.lastLineWasDualDialogue = isDualDialogue;
+
+        return [...previous, ...dialogueTokens];
+    }
+
+    private tokenizeSection(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.section);
+        const sectionToken = { type: 'section', text: match[2], depth: match[1].length };
+        return [...previous, sectionToken];
+    }
+
+    private tokenizeSynopsis(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.synopsis);
+        const synopsisToken = { type: 'synopsis', text: match[1] };
+        return [...previous, synopsisToken];
+    }
+
+    private tokenizeNote(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.note);
+        const noteToken = { type: 'note', text: match[1] };
+        return [...previous, noteToken];
+    }
+
+    private tokenizeBoneyard(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.boneyard);
+        const boneyardToken = { type: match[0][0] === '/' ? 'boneyard_begin' : 'boneyard_end' };
+        return [...previous, boneyardToken];
+    }
+
+    private tokenizeLyrics(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.lyrics);
+        const lyricToken = { type: 'lyrics', text: match[0].replace(/^~(?![ ])/gm, '') };
+        return [...previous, lyricToken];
+    }
+
+    private tokenizePageBreak(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.page_break);
+        const pageBreakToken = { type: 'page_break' };
+        return [...previous, pageBreakToken];
+    }
+
+    private tokenizeLineBreak(line: string, previous: Token[]): Token[] {
+        const match = line.match(regex.line_break);
+        const lineBreakToken = { type: 'line_break' };
+        return [...previous, lineBreakToken];
+    }
+
+    private tokenizeAction(line: string, previous: Token[]): Token[] {        
+        const actionToken = { type: 'action', text: line.replace(/^!(?![ ])/gm, '') };
+        return [...previous, actionToken];
+    }
+
+    private isTooShort(str: string) {
+        return str.indexOf('  ') === str.length - 2
     }
 }
